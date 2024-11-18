ae17f97361ae4ea0c958ba8b315c9d98a3590190
Wasim9Daoud <wasimdaoud05@gmail.com>
2024-11-18 10:08:07 +0300

    fix layout path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Children } from "react";
-import Layout from "./components/layout";
+// import Layout from "./components/layout";
+import Layout from "../src/components/Layout.jsx"
 
 function App() {
   return <Layout>{Children}</Layout>;
