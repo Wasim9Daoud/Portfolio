65c13c1cb805bb1868b1a7f5e95c9352a141774f
Wasim9Daoud <wasimdaoud05@gmail.com>
2024-11-25 09:49:02 +0300

    update images source

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -5,8 +5,7 @@ export const projects = [
     id : 1 ,
     type : "Full Stack" ,
     title: "Social-App",
-    picture:
-      "https://d32zpt07is85ts.cloudfront.net/2024_11_18_f590a71c-99dd-4dee-a74f-489f37f5ab69?Expires=1732081843&Signature=tgd~XcbrO1yiZ5G2ruLM5hx-o-C1k4q-SUJhvz7Ni4XueTKze~nW~14gU91Cf5tX36YkgxgOtxk8fA-DeUnvqzkPt67F1X-QWRrpi06q5GKK6hdl3jRelxnvrzmBw91dP9ygtc-qMtEQWBDs-lS4bxeaoJ14EmUfIM-cMAsZFxKREQLIHV9cb1Ff2XXeSPSTr30zXDQGcTjm4BYkUS5AeYbh1eE9UGp8vGowiNsdY6opicobsTKhoM4u0jyezEx5Bk5RGlNVtzJ5NoCeV6D3ZR18JB6T7ep-wsnmsequO21wP5Kfa4c~JEbLmRGWF0J2USDWw5ChzdsP-m6OKZ9CrQ__&Key-Pair-Id=KTUQ5O0UDZ92B",
+    picture: "https://github.com/Wasim9Daoud/Portfolio/blob/main/src/assets/SocialApp.png" ,
     description: `Social-App is a responsive application, contains many different pages ( login register Home profile postDetails Dashboard  (users-table posts-table comments-table )) and an authentication system ,a verify system for check emails ,more powers have been distributed to Admin user.. any user can browse the app but only logged-in users can create & update ( profiles , posts , comments ) , only admin-user can access to the Dashboard he can just delete without updating ,user also can add images to his profile or posts`,
 
     skillsUsed: [
@@ -32,8 +31,7 @@ export const projects = [
     id : 2 ,
     type : "Full Stack" ,
     title: "Cloud-Hosting",
-    picture:
-      "https://d32zpt07is85ts.cloudfront.net/2024_11_18_6d85283e-4ab9-4c3a-805d-77eed16a0857?Expires=1732082498&Signature=ZcHWj1AosPu-w~W5inYbYZUedK2lPF5iBYcgqN-rzxi4jDB8Ki2MT8I---IVy-uqrr6BOFFgYgBYXhwVnSmewV9hkMXIMoNLIHupkYqyl0MdZaftp6fAcpnYPrPkzobBrcigbVxko2n6c~pB4jZ1qY6YkZgpHx2kpJdb3g5p9A2Xt~0I8vSZkIohS1UjiSUr84QZmioyCqzwsE6R~G-FC5WZYpWWcyiwUWXr22xJAYX0818VPwjQtuGPeYtr~yMCiyztv8MJPRkRVQlrvEeaLRw5L~87vMjzYUiqL7x5vx1mLbgt4F-FrtF0aBtR5NlHs~Fc~~mapEWGnxBGeJ9Tjg__&Key-Pair-Id=KTUQ5O0UDZ92B",
+    picture: "https://github.com/Wasim9Daoud/Portfolio/blob/main/src/assets/CloudHosting.png" ,
     description: `Cloud-Hosting is a responsive application, contains many different pages ( login register Home Posts postDetails Dashboard  (users-table posts-table comments-table )) and an authentication system more powers have been distributed to Admin user.. any user can browse the app but only logged-in users can create & update ( posts , comments ) , only admin-user can access to the Dashboard he can just delete users , posts or comments without updating `,
 
     skillsUsed: [
@@ -58,8 +56,7 @@ export const projects = [
     id : 3 ,
     type : "Front-End" ,
     title: "Tasty Restaurant",
-    picture:
-      "https://d32zpt07is85ts.cloudfront.net/2024_11_18_3d433da9-c39a-4537-975b-58769367d387?Expires=1732082549&Signature=cV~PrjCY9acv8hy6ECjFjXYqamz7B78zGqmmhkgZbcldh-DYzkDWnvn3shL0I3bWVaSQNZ0OV5v-jOAKTxBaH8O-Abg9KXsk-3Tvduv1XMbbCrdZKIgi2vx0NUjCjGAJD9GvQ1xEHikRyLzms8geXkx9zFl863UshkzLHvIBWe6nAD62GeT9XpGlulfwb7grrCR~wk6PVnMTBurZZVCBqAPctWrumxpAUFds~cynP82~o1CvQnCSmRajUGWEdm4Y~hNH8nbMoQcyR8yzTunfxm4nopGuralmFHrnsweLISnLGGqE0fSYoS6Ep3rKxUowOpd6cqGC0S19yOhcZeV7YA__&Key-Pair-Id=KTUQ5O0UDZ92B",
+    picture:"https://github.com/Wasim9Daoud/Portfolio/blob/main/src/assets/Restaurant.png" ,
     description: `Tasty Restaurant is a responsive application, contains many different pages ( login register Home meals-kinds mealDetails Cart ), user can select kind of foods & meals & drinks.. there is cart page contains all meals&drinks and the total price  `,
 
     skillsUsed: [
@@ -87,8 +84,7 @@ export const projects = [
     id : 4 ,
     type : "Front-End" ,
     title: "Weather",
-    picture:
-      "https://d32zpt07is85ts.cloudfront.net/2024_11_18_a8e724ad-dbd5-4873-8465-bb9f34abd787?Expires=1732082657&Signature=I5EHgOpxTqX-X67bf2ZkNA-Iza8eCYT4B03oplC6yEg0atsKVuiLxpFbwsLf~sirPefignZbAGKOxxoKwh7fIvu4HNae~c0c2KlnzRkL4p0z-P1SKkQMaAeSlicypRO-SfEPc091LtYesdVzMobR5UojJIbmrHV907K8LSGZhi1rkI~9hRtEevPxuZfQBvzzNyUbSy9ZwobOKSN-neAgweLGoZlWTD-Cf3Wu1wluBbcU20HSk2~3jVN1xhIqdnSfnIW0zlifqRnjMRcKpdH7omiYADSyVvdgvUv5Av5tp3edugEh1rtobl0Oq6xkNmjFxtdyfvJDbtKuoT2wAmj0uQ__&Key-Pair-Id=KTUQ5O0UDZ92B",
+    picture: "https://github.com/Wasim9Daoud/Portfolio/blob/main/src/assets/weather.png" ,
     description: `Weather App is a simple application, contains one page , user can write name of city to get the weather`,
 
     skillsUsed: [
